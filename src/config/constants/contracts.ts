73d223653d647965dda9af298b79b78c572ab71d
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-09 18:19:06 +0700

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,14 +1,14 @@
 export default {
   cake: {
-    56: '0x019bE1796178516e060072004F267B59a49A0801', //token contract
+    56: '0x019bE1796178516e060072004F267B59a49A0801',
     97: '',
   },
   masterChef: {
-    56: '0xdfc263b5dd1703a44e5a15f2d65b6062919ba74d', //masterchef
+    56: '0xdfc263b5dd1703a44e5a15f2d65b6062919ba74d',
     97: '',
   },
   wbnb: {
-    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', //wbnb
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     97: '',
   },
   lottery: {
@@ -20,11 +20,11 @@ export default {
     97: '',
   },
   mulltiCall: {
-    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb', //multicall?
-    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412', //fee address?
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
   },
   busd: {
-    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56', //busd
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
     97: '',
   },
 }
