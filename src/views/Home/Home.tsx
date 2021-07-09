358c99354db6254ce1ebae13f73d7e6e2e7a0ae0
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-09 18:06:58 +0700

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,7 +60,7 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'ZCore Finance (ZEFI)')}
+          {TranslateString(576, 'ZCore2 Finance (ZEFI)')}
         </Heading>
         <Text>{TranslateString(578, 'The Farm DEFI token on Binance Smart Chain.')}</Text>
       </Hero>
